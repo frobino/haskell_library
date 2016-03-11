2421504b029c0065544fe39d6b48ec2915b1c9f3
frobino <frobino@users.noreply.github.com>
2016-03-11 22:04:29 +0100

    [DOC] improved comments in Main.hs of JSON project

--- examples-libraries/JSON-manipulation-library/Main.hs
+++ examples-libraries/JSON-manipulation-library/Main.hs
@@ -1,4 +1,8 @@
 -- the Main module is NEEDED to create an executable through "ghc <filename.hs>"
+--
+-- NOTE: "print" is a std function in the std lib in Haskell, and we can print JOblects (which is of type JValue)
+--       becase JValue has been derived from Show (see SimpleJSON.hs)
+
 module Main where
 
 import SimpleJSON
